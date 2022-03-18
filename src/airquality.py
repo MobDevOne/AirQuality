a19095228fb69ebd92700eb35cf7bf1907111e08
Fotiadis, Spiridon <Spiridon.Fotiadis@gdata.de>
2022-03-18 15:05:49 +0100

    extend error handling

--- src/airquality.py
+++ src/airquality.py
@@ -1,9 +1,13 @@
 import logging
 from datetime import datetime, timedelta
 
-import pandas as pd
-from pandas import errors
+from urllib.error import URLError
+import sqlalchemy
+
 import mariadb
+import pandas as pd
+from pandas.io.sql import DatabaseError
+
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
@@ -12,30 +16,28 @@ class AirQuality:
     def __init__(self, particle_sensor_id, humidity_sensor_id):
         self.sds_id = particle_sensor_id
         self.dht_id = humidity_sensor_id
-        self._sds_df = None
-        self._dht_df = None
 
     def connect_database(self):
         conn = None
         try:
             conn = mariadb.connect(
-                user = "example-user",
-                password = "my_cool_secret",
-                host = "127.0.0.1",
-                port = 3306,
-                database = "AirQuality"
+                user="example-user",
+                password="my_cool_secret",
+                host="127.0.0.1",
+                port=3306,
+                database="AirQuality"
             )
             logging.info("Successfully conected to database.")
+        except mariadb.Error as mariadb_error:
+            logging.error(f"Error connecting to MariaDB Platform: {mariadb_error}")
 
-            return conn
-        except mariadb.Error as error:
-            logging.error(f"Error connecting to MariaDB Platform: {error}")
-            return conn
+        return conn
 
-    def import_data(self, days=1):
-        connection = self.connect_database()
-        for day in range(days):
-            now = datetime.now()-timedelta(days=day)
+    def import_data(self, period=1):
+        engine = sqlalchemy.create_engine("mariadb+mariadbconnector://example-user:my_cool_secret@127.0.0.1:3306")
+
+        for days_counter in range(period):
+            now = datetime.now()-timedelta(days=days_counter)
             date = now.strftime("%Y-%m-%d")
 
             sds_url = f'http://archive.sensor.community/{date}/{date}_sds011_sensor_{self.sds_id}.csv'
@@ -43,30 +45,32 @@ class AirQuality:
             dht_url = f'http://archive.sensor.community/{date}/{date}_dht22_sensor_{self.dht_id}.csv'
 
             logging.debug(f"Importing data for {date}")
-
             try:
-                self._sds_df = pd.read_csv(sds_url, sep=';')
-                self._sds_df.dropna(how='all', axis=1, inplace=True)
-                self._sds_df.to_sql("sds_sensor", connection, if_exists="append")
+                sds_df = pd.read_csv(sds_url, sep=';')
+
+                sds_df.dropna(how='all', axis=1, inplace=True)
                 
-            except errors.ParserError as e:
-                logging.warning(f"could not read sds data for {date}: {e}")
+                #sds_df.to_sql("sds_sensor", engine.raw_connection, if_exists="append")
+            except DatabaseError as db_error:
+                logging.error(f"a database error occoured: {db_error}")
+            except URLError as url_error:
+                logging.error(f"could not read sds data for {date}: {url_error}")
+            except Exception as ex:
+                logging.exception(f"something went wrong: {ex}")
 
             try:
-                self._dht_df = pd.read_csv(dht_url, sep=';')
-                self._dht_df.dropna(how='all', axis=1, inplace=True)
-                self._sds_df.to_sql("dht_sensor", connection, if_exists="append")
-            except errors.ParserError as e:
-                logging.warning(f"could not read dht data for {date}: {e}")
+                dht_df = pd.read_csv(dht_url, sep=';')
 
-    def get_particle_dataframe(self):
-        return self._sds_df
+                dht_df.dropna(how='all', axis=1, inplace=True)
 
-    def get_humidity_dataframe(self):
-        return self._sds_df
-
-
+                #sds_df.to_sql("dht_sensor", engine.raw_connection, if_exists="append")
+            except DatabaseError as db_error:
+                logging.error(f"a database error occoured: {db_error}")
+            except URLError as url_error:
+                logging.error(f"could not read dht data for {date}: {url_error}")
+            except Exception as ex:
+                logging.exception(f"something went wrong: {ex}")
 
 if __name__ == "__main__":
     location = AirQuality(3659, 3660)
-    location.import_data(3)
+    location.import_data(5)
